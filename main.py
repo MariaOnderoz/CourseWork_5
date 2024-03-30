b47f568dcd82ee557ad6ced3e89e8dd8566cf002
mariaonderoz <mskorneeva.87@gmail.com>
2024-03-30 17:40:18 +0700

    added user_interaction

---
+++ main.py
@@ -0,0 +1,38 @@
+from db_manager import DBManager
+from utils import create_database, create_tables, save_data_to_database
+
+def user_interaction():
+
+    list_employers = [1942330, 49357, 78638, 2748, 3529, 9498112, 93787326, 2180, 1648566, 1942336]
+    dbm = DBManager()
+    create_database('db_name')
+    create_tables('db_name')
+    insert_data_into_tables(list_employers)
+
+    while True:
+
+        new = input(
+            '1 - список всех компаний и кол-во вакансий у каждой компании\n'
+            '2 - список всех вакансий с указанием названия компании, вакансии, зарплаты и ссылки на вакансию\n'
+            '3 - средняя зарплата по вакансиям\n'
+            '4 - список всех вакансий, у которых зарплата выше средней по всем вакансиям\n'
+            '5 - список вакансий, в названии которых содержится ключевое слово\n'
+            'Выход - закончить\n'
+        )
+
+        if new == '1':
+            print(dbm.get_companies_and_vacancies_count())
+        elif new == '2':
+            print(dbm.get_all_vacancies())
+        elif new == '3':
+            print(dbm.get_avg_salary())
+        elif new == '4':
+            print(dbm.get_vacancies_with_higher_salary())
+        elif new == '5':
+            keyword = str(input('Найти: '))
+            print(dbm.get_vacancies_with_keyword(keyword))
+        elif new == 'Выход':
+            break
+
+if __name__ == "__main__":
+    user_interaction()
